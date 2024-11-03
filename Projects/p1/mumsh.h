87afc61f057182fa3e6544b6e370133328b1b3e6
jinlock <jinlock99@sjtu.edu.cn>
2024-11-03 15:18:59 +0800

    feat: project 1

---
+++ Projects/p1/mumsh.h
@@ -0,0 +1,16 @@
+#ifndef MUMSH_H
+#define MUMSH_H
+
+#define MAX_LINE 1024
+#define MAX_PIPE 100
+#define MAX_PATH 250
+#define MAX_JOBS 100
+
+#define FALSE 0
+#define DONE 1
+#define RUN 2
+
+#define PROMPT "mumsh $ "
+#define ENTER "> "
+
+#endif
